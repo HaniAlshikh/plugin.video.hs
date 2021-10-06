750e72ec3b94e78e2bae7233f64895d4877830a2
Hani Alshikh <39484930+HaniAlshikh@users.noreply.github.com>
2021-10-06 03:50:01 +0200

    added arabseed (#4)

---
+++ resources/lib/modules/providers/arabseed.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+
+import re
+from typing import TYPE_CHECKING
+
+import bs4
+
+from resources.lib.common.exceptions import try_and_log
+from resources.lib.modules.globals import g
+from resources.lib.modules.metadata_handler import MetadataHandler
+from resources.lib.modules.providers.provider_utils import get_quality
+
+from bs4 import BeautifulSoup
+from resources.lib.modules.providers.provider import Provider
+
+import os
+import sys
+
+
+class Arabseed(Provider):
+    def __init__(self):
+        super().__init__(
+            'عرب سيد',
+            "arabseed",
+            ["https://arabseed.onl/"],
+        )
+
+    def get_movies_categories(self):
+        return self._get_navbar_element('main/', 2)
+
+    def get_movies_list(self, category: str):
+        return self._get_posts('category/' + category, g.MEDIA_MOVIE)
+
+    def get_shows_categories(self):
+        return self._get_navbar_element('main/', 4)
+
+    def get_shows_list(self, category: str):
+        return self._get_posts('category/' + category, g.MEDIA_SHOW)
+
+    def get_shows_seasons(self, url: str):
+        return []
+
+    def get_season_episodes(self, url: str):
+        posts = []
+        response = self.requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        from collections import defaultdict
+        for postDiv in soup.select_one('div.ContainerEpisodesList').find_all('a'):
+            post = defaultdict(dict)
+
+            post['info']['title'] = postDiv.get_text()
+            post['art']['poster'] = soup.select_one('div.Poster').find('img').get('src')
+            post['info']['mediatype'] = g.MEDIA_EPISODE
+
+            post['url'] = postDiv.get('href')
+            post['provider'] = self.name
+
+            post['args'] = g.create_args(post)
+            posts.append(post)
+        return posts
+
+    def search(self, query: str, mediatype: str):
+        type = 'series' if mediatype == g.MEDIA_SHOW else 'movie'
+        response = self.requests.post(
+            'wp-content/themes/Elshaikh2021/Ajaxat/SearchingTwo.php',
+            {'search': query, 'type': type}
+        )
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return self._get_posts('', mediatype, soup)
+
+    def get_sources(self, url: str):
+        sources = []
+        response = self.requests.get(url + 'download/')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for source in soup.find('div', class_="DownloadArea").find_all("a", class_='downloadsLink'):
+            quality = get_quality(source.p.get_text())
+            provider = source.span.get_text()
+            source = {'display_name': provider + ' ' + quality,
+                      'release_title': soup.find('title').get_text(),
+                      'url': source.get('href').strip(),
+                      'quality': quality,
+                      'type': 'hoster',
+                      'provider': provider,
+                      'origin': self.name}
+            MetadataHandler.improve_source(source)
+            sources.append(source)
+        return sources
+
+    def _get_posts(self, page: str, mediatype: str, soup: bs4.BeautifulSoup = None) -> list:
+        posts = []
+        page = "{}/?page={}".format(page, g.PAGE) if g.PAGE > 1 else page
+        if not soup:
+            response = self.requests.get(page)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+        from collections import defaultdict
+        duplicates = {}  # used mostly to filter episodes
+        for postDiv in soup.select_one('ul.Blocks-UL').find_all('a'):
+            poster_img_tag = postDiv.find('img')
+            poster = poster_img_tag.get(
+                next((k for k in ['data-src', 'data-image', 'src'] if poster_img_tag.get(k)), None))
+            if mediatype == g.MEDIA_SHOW and duplicates.get(poster):
+                continue
+
+            post = defaultdict(dict)
+
+            post['info']['title'] = postDiv.find('h4').get_text()
+            post['art']['poster'] = poster
+            post['info']['plot'] = postDiv.select_one('div.Story').get_text()
+            post['info']['mediatype'] = mediatype
+
+            post['url'] = postDiv.get('href')
+            post['provider'] = self.name
+
+            if mediatype == g.MEDIA_SHOW:
+                self._improve_show_meta(post)
+
+            post['args'] = g.create_args(post)
+            posts.append(post)
+            duplicates[poster] = post
+
+        page = self._get_current_page_number(soup)
+        if page:
+            posts.append(page + 1)
+
+        return posts
+
+    def _improve_show_meta(self, show):
+        show['info']['title'] = show['info']['title'].split('الحلقة')[0]
+        show['info']['plot'] = '' # it's not really useful here
+
+    def _get_navbar_element(self, page, number: int) -> list:
+        elements = []
+        response = self.requests.get(page)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for el in soup.select_one('ul#main-menu').find_all('li', recursive=False)[number].ul.find_all('a'):
+            elements.append(el.get_text())
+        return elements
+
+    @staticmethod
+    def _get_current_page_number(soup):
+        pages = soup.find('ul', class_='page-numbers')
+        if pages:
+            page = pages.select_one('li.active > a')
+            if not page:
+                page = pages.select_one('span.current')
+            if page:
+                g.log('Current Page: ' + page.get_text())
+                return int(page.get_text())
+
+        return None
--- resources/lib/modules/request.py
+++ resources/lib/modules/request.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, unicode_literals
 
+from urllib.parse import urlparse
+
 import requests
 
 from resources.lib.modules.globals import g
@@ -11,9 +13,21 @@ class Request:
         self.base = base
 
     def get(self, page: str = ''):
-        page = self.base + page if self.base not in page else page
-        page = page.replace(' ', '+') if '?s' or 'search' in page else page.replace(' ', '-')
+        page = self.base + page if urlparse(self.base).netloc not in page else page
+        page = page.replace(' ', '+') if any(s in page for s in ['?s', 'search']) else page.replace(' ', '-')
         g.log('GET: Requesting: ' + page)
         r = requests.get(page)
         r.encoding = 'utf-8'
         return r
+
+    def post(self, page: str = '', data: dict = {}):
+        page = self.prep_url(page)
+        g.log('POST: Requesting: ' + page)
+        r = requests.post(page, data)
+        r.encoding = 'utf-8'
+        return r
+
+    def prep_url(self, page):
+        page = self.base + page if urlparse(self.base).netloc not in page else page
+        page = page.replace(' ', '+') if any(s in page for s in ['?s', 'search']) else page.replace(' ', '-')
+        return page
